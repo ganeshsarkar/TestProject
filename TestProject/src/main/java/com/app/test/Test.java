b71b276ed532d5e384dbe74f84659712a29b6c56
ganesh <ganesh@192.168.0.23>
2018-12-02 23:52:30 +0530

    #365 Test 2nd Time Commit

--- TestProject/src/main/java/com/app/test/Test.java
+++ TestProject/src/main/java/com/app/test/Test.java
@@ -3,8 +3,9 @@ package com.app.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test Display");
+		System.out.println("Test Display 1st time ");
 
+		System.out.println("Test Display 2nd time");
 	}
 
 }
